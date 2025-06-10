8f9ce3d71a557d4d7adbe27973d7ebd984772da7
Vinicius Sanches <vpsanches00@gmail.com>
2025-06-10 00:16:36 -0300

    criando o componente upload

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories"
 import { Input } from "../components/input"
 import { Select } from "../components/Select"
+import { Upload } from "../components/upload"
 
 export function Refund() {
     const [category, setCategory] = useState("")
@@ -10,21 +11,31 @@ export function Refund() {
     return(
         <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
             <header>
-                <h1 className="text-xl font-bold text-gray-900">
+                <h1 className="text-xl font-bold text-gray-100">
                     Solicitação de reembolso
                 </h1>
 
-                <p className="text-sm text-gray-800 mt-2 mb-4">
+                <p className="text-sm text-gray-200 mt-2 mb-4">
                     Dados da despesa para solicitar reembolso
                 </p>
             </header>
 
+            
             <Input required legend="Nome da Solicitação"/>
-            <Select required legend="Categoria" value={category} onChange={(e) => setCategory(e.target.value)}>
-                {CATEGORIES_KEYS.map((category) => (
-                        <option>{CATEGORIES[category].name}</option>
-                ))}
-            </Select>
+
+            <div className="flex gap-4">
+                <Select required legend="Categoria" value={category} onChange={(e) => setCategory(e.target.value)}>
+                    {CATEGORIES_KEYS.map((category) => (
+                            <option>{CATEGORIES[category].name}</option>
+                    ))}
+                </Select>
+
+                <Input legend="Valor" required />
+            </div>
+
+            <Upload />
+
+
         </form>
     )
 }
